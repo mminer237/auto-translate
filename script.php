6659507aa354646f011771de36417789100a9e0f
Matthew Miner <mminer237@gmail.com>
2021-09-17 07:54:33 -0500

    Read input

--- script.php
+++ script.php
@@ -17,5 +17,46 @@
 	$options = getopt('hi:o:');
 	if (isset($options['h']))
 		exit(file_get_contents('help.txt'));
-	$input = $options['i'];
-	$output = $options['i'];
+	$input_path = $options['i'];
+	$output_path = $options['o'];
+	if (!isset($input_path) && !isset($output_path) && count($argv) >= 4) {
+		$input_path = $argv[2];
+		$output_path = $argv[3];
+	}
+	else
+		exit('Input and output not set');
+
+	foreach (['input', 'output'] as $direction) {
+		if (strlen(${$direction.'_path'}) > 5) {
+			if (substr(${$direction.'_path'}, -5) === '.json') {
+				${$direction.'_type'} = 'json';
+				if ($direction === 'input')
+					$input = json_decode(file_get_contents(${$direction.'_path'}));
+			}
+			elseif (
+				substr(${$direction.'_path'}, -5) === '.yaml' ||
+				substr(${$direction.'_path'}, -4) === '.yml'
+			) {
+				${$direction.'_type'} = 'json';
+				if ($direction === 'input')
+					$input = Yaml::parseFile(${$direction.'_path'});
+			}
+			elseif (
+				substr(${$direction.'_path'}, -3) === '.db' ||
+				substr(${$direction.'_path'}, -7) === '.sqlite' ||
+				substr(${$direction.'_path'}, -8) === '.sqlite3'
+			) {
+				exit("Databases are not supported");
+			}
+			else {
+				if ($direction === 'input') {
+					$input_type = 'txt';
+					$input = file_get_contents($input_path);
+				}
+				else {
+					$output_type = $input_type;
+				}
+				
+			}
+		}
+	}

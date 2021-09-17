56a5d7ae64bdf527e466f5b18af4396a72a044ec
Matthew Miner <mminer237@gmail.com>
2021-09-17 07:06:56 -0500

    Read API keys

--- script.php
+++ script.php
@@ -6,3 +6,10 @@
 	use Symfony\Component\Yaml\Yaml;
 
 	chdir(dirname(__FILE__));
+
+	$api_keys_file = 'api_keys.yaml';
+	if (file_exists($api_keys_file))
+		$api_keys = Yaml::parseFile($api_keys_file);
+	else {
+		exit("No API keys file found");
+	}
